f387a42a3d966b1e0011a54ff119519aadd2f251
katepass <katepass@iastate.edu>
2026-09-14 14:40:52 -0500

    Update CreateTables.sql formatting

--- CreateTables.sql
+++ CreateTables.sql
@@ -10,26 +10,26 @@ CREATE TABLE products (
 	product_id INT,
     product_name TEXT,
     category TEXT,
-    unit_pice DOUBLE,
+    unit_price DOUBLE,
 	PRIMARY KEY(product_id)
 );
 
 CREATE TABLE orders (
-order_id INT,
-order_date DATETIME,
-quantity INT,
-discount DOUBLE,
-payment_method TEXT,
-status TEXT,
-PRIMARY KEY(order_id),
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-FOREIGN KEY(product_id) REFERENCES products(product_id)
+	order_id INT,
+	order_date DATETIME,
+	quantity INT,
+	discount DOUBLE,
+	payment_method TEXT,
+	status TEXT,
+	PRIMARY KEY(order_id),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY(product_id) REFERENCES products(product_id)
 );
 
 CREATE TABLE payments(
-payment_id INT,
-payment_date DATETIME,
-payment_status TEXT,
-PRIMARY KEY(payment_id),
-FOREIGN KEY (order_id) REFERENCES orders(order_id)
+	payment_id INT,
+	payment_date DATETIME,
+	payment_status TEXT,
+	PRIMARY KEY(payment_id),
+	FOREIGN KEY (order_id) REFERENCES orders(order_id)
 )
